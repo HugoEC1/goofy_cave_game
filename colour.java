3e9b023693058e7e67537c7f7026df7a9b0c93fe
HugoEC1 <90981165+HugoEC1@users.noreply.github.com>
2023-07-18 20:18:09 -0400

    Add files via upload

---
+++ colour.java
@@ -0,0 +1,6 @@
+public class colour {
+    public static final String RESET = "\u001b[0m";
+    public static final String RED = "\u001b[31m";
+    public static final String BLUE = "\u001b[34m";
+    public static final String GRENN = "\u001b[32m";
+}
