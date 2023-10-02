8ab23db88a29b96fc77c763ed073bf6944a7c697
BuildTools <unconfigured@null.spigotmc.org>
2023-10-02 00:16:03 -0400

    epic

---
+++ GameSettings.cs
@@ -0,0 +1,6 @@
+﻿namespace CaveGame;
+static class GameSettings
+{
+    public const int GAME_WIDTH = 90;
+    public const int GAME_HEIGHT = 30;    
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// :3
+
+namespace CaveGame
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Settings.WindowTitle = "Goofy Cave Game";
+
+            Game.Configuration gameStartup = new Game.Configuration()
+                .SetScreenSize(GameSettings.GAME_WIDTH, GameSettings.GAME_HEIGHT)
+                .SetStartingScreen<CaveGame.Scenes.StartScreen>();
+
+            Game.Create(gameStartup);
+            Game.Instance.FrameUpdate += Update;
+            Game.Instance.Run();
+            Game.Instance.Dispose();
+
+        }
+
+        private static void Update(object? sender, GameHost e)
+        {
+            System.Console.WriteLine(e);
+        }
+    }
+}
