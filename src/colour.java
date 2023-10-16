e0e7db7a14ac9554ac9185d35153ef6abe26061a
Hugo Chisholm <90981165+HugoEC1@users.noreply.github.com>
2023-10-16 13:49:36 +0000

    deleted poo poo

--- src/colour.java
+++
@@ -1,7 +0,0 @@
-public class colour {
-    public static final String RESET = "\u001b[0m";
-    public static final String RED = "\u001b[31m";
-    public static final String BLUE = "\u001b[34m";
-    public static final String GREEN = "\u001b[32m";
-	public static final String YELLOW = "\u001b[33m";
-}
