a89e33697c8a14d3fb4b3342a5e3520624eec9d4
Hugo Chisholm <90981165+HugoEC1@users.noreply.github.com>
2023-10-18 17:02:30 +0000

    Enemy manager converted to c#

--- obj/Debug/net6.0/CaveGame.AssemblyInfo.cs
+++ obj/Debug/net6.0/CaveGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaveGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0295c76fb3284bd8a8cf17be4e6a85a69f0035ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaveGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaveGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
